6482d10f29fdf784637044459ddfbdf5eb282ce0
sanyath007 <sanyath007@gmail.com>
2021-04-20 11:19:37 +0700

    import models/Bed to PatientController and use try/catch to handle error of store method

--- src/Controllers/PatientController.php
+++ src/Controllers/PatientController.php
@@ -6,6 +6,7 @@ use App\Controllers\Controller;
 use Illuminate\Database\Capsule\Manager as DB;
 use App\Models\Patient;
 use App\Models\Registration;
+use App\Models\Bed;
 
 class PatientController extends Controller
 {
@@ -39,35 +40,47 @@ class PatientController extends Controller
     {
         $post = (array)$request->getParsedBody();
 
-        $patient = new Patient;
-        $patient->hn = $post['hn'];
-        $patient->name = $post['name'];
-        $patient->sex = $post['sex'];
-        $patient->age_y = $post['age_y'];
-        $patient->tel = $post['tel'];
+        try {
+            $patient = new Patient;
+            $patient->hn = $post['hn'];
+            $patient->cid = $post['cid'];
+            $patient->name = $post['name'];
+            $patient->sex = $post['sex'];
+            $patient->birthdate = $post['birthdate'];
+            $patient->age_y = $post['age_y'];
+            $patient->tel = $post['tel'];
 
-        if($patient->save()) {
-            $reg = new Registration;
-            $reg->an = $post['an'];
-            $reg->hn = $post['hn'];
-            $reg->reg_date = $post['reg_date'];
-            $reg->ward = $post['ward'];
-            $reg->bed = $post['bed'];
-            $reg->lab_date = $post['lab_date'];
-            $reg->lab_result = $post['lab_result'];
-            $reg->dx = $post['dx'];
-            $reg->symptom = $post['symptom'];
-            $reg->reg_from = $post['reg_from'];
-            $reg->reg_state = $post['reg_state'];
-            $reg->remark = $post['remark'];
-            $reg->save();
+            if($patient->save()) {
+                $reg = new Registration;
+                $reg->an = $post['an'];
+                $reg->hn = $post['hn'];
+                $reg->reg_date = $post['reg_date'];
+                $reg->ward = $post['ward'];
+                $reg->bed = $post['bed'];
+                $reg->lab_date = $post['lab_date'];
+                $reg->lab_result = $post['lab_result'];
+                $reg->dx = $post['dx'];
+                $reg->symptom = $post['symptom'];
+                $reg->reg_from = $post['reg_from'];
+                $reg->reg_state = $post['reg_state'];
+                $reg->remark = $post['remark'];
+                $reg->save();
 
-            Bed::where('bed_id', $post['bed'])->update(['bed_status' => 1]);
+                Bed::where('bed_id', $post['bed'])->update(['bed_status' => 1]);
 
-            return $response->withStatus(200)
+                return $response->withStatus(200)
+                        ->withHeader("Content-Type", "application/json")
+                        ->write(json_encode($patient, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+            }
+        } catch (\Exception $ex) {
+            return $response
+                    ->withStatus(500)
                     ->withHeader("Content-Type", "application/json")
-                    ->write(json_encode($patient, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
-        }                    
+                    ->write(json_encode([
+                        'status' => 0,
+                        'message' => $ex->getMessage()
+                    ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+        }
     }
 
     // public function update($request, $response, $args)
